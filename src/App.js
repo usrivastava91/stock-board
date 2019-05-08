1ddf67f442d6c91f9775202a03c908a9331318a4
utkarsh srivastava <utkarsh.x.srivastava.consultant@nielsen.com>
2019-05-08 17:27:45 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import openSocket from 'socket.io-client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+// const socket = openSocket('ws://stocks.mnet.website');
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    // socket.on('message', (message) => {
+    //   console.log('RECIEVING ============', message)
+    // })
+  }
+  componentDidMount = () => {
+    this.connection = new WebSocket('ws://stocks.mnet.website');
+    this.connection.onmessage = this.connectionworking;
+
+  }
+
+  connectionworking = (message) => {
+console.log('RECIEVING ============', message.data)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
